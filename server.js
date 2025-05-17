8c76f0891632ece5817fd458435532a52eacc7cd
Satyam Rajput <satyamrajput081998@gmail.com>
2025-05-17 03:34:36 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,5 @@
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+console.log("WELCOME TO BACKEND");
+app.listen("7001", () => console.log("Connected at 7001 Port"));
